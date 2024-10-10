199ab411f97d2a5d4d8835a7ad890bb89a5c39ed
r12357 <dxdy9955@gmail.com>
2024-10-10 09:58:29 +0900

    first comit

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("prac00.tsの実行");
+require("./prac00");
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+console.log("prac00.tsの実行");
+import "./prac00";
